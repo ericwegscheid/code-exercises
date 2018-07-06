51e21f3295ce128d7a0237a36c3e594a926d538e
Eric Wegscheid <{ericwegscheid@gmail.com}>
2018-07-06 15:53:05 -0500

    added find cycle in array example for python, made whitespace change to node finde cycle in array example

---
+++ python/find-cycle-in-array.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+arr = [3, 2, 3, 1, 2] # result: [3, 1, 2]
+# arr = [3, 2, 4, 2, 1] # result: [2, 4, 1]
+# arr = [3, 2, 4, 5, 1] # result: No cycle found
+# arr = [1, 2, 3, 4, 5] # result: No cycle found
+
+cycle = []
+
+def step(i):
+    match = cycle.index(i) if i in cycle else -1
+    v = arr[i] if len(arr) >= i + 1 else None
+
+    if v is None:
+        return 'No cycle found'
+
+    if match >= 0:
+        return cycle[match:]
+
+    cycle.append(i)
+
+    return step(v)
+
+print(step(0))
