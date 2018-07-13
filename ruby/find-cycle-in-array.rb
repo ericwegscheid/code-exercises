d1ce42a338848afb54ca6d9f6796f9bf75122efb
Eric Wegscheid <ericwegscheid@gmail.com>
2018-07-12 22:33:05 -0500

    added find cycle in array for ruby (still needs work)

---
+++ ruby/find-cycle-in-array.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/ruby
+
+$arr = [3, 2, 3, 1, 2] # result: [3, 1, 2]
+# arr = [3, 2, 4, 2, 1] # result: [2, 4, 1]
+# arr = [3, 2, 4, 5, 1] # result: No cycle found
+# arr = [1, 2, 3, 4, 5] # result: No cycle found
+
+$cycle = []
+
+
+# @TODO: doesn't work yet fix it
+
+def step(i)
+	match = $cycle.index(i)
+	v = $arr[i]
+
+	if !v
+		return 'No cycle found'
+	end
+
+	if match
+		return $cycle[match]
+	end
+
+	$cycle << i
+
+	return step(v)
+end
+
+puts step(0)
