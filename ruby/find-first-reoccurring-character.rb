7028a9a41ce888673afbf0e8132288da96b0ad2b
Eric Wegscheid <ericwegscheid@gmail.com>
2018-07-17 22:13:18 -0500

    added ruby example for find first reoccurring character in string

---
+++ ruby/find-first-reoccurring-character.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+
+$str = 'dabcfa'; # result: a
+# $str = 'adbdfa'; # result: d
+# $str = 'dabcfe'; # result: No reoccurring characters found
+
+$track = {}
+$result = 'No reoccurring characters found'
+
+$str.each_char do |character|
+	if $track.has_key? character
+		$result = character
+		break
+	end
+
+	$track[character] = true
+end
+
+puts $result
