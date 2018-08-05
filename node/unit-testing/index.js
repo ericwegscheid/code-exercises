24bf18761656f019122085b8e3019b251a63a577
Eric Wegscheid <ericwegscheid@gmail.com>
2018-08-04 20:04:01 -0500

    added minimalist TDD example to node

---
+++ node/unit-testing/index.js
@@ -0,0 +1,6 @@
+var Bear = require('./src/bear');
+var bear = new Bear();
+
+console.log(bear.speak());
+console.log(bear.ask('What is your favorite food?'));
+console.log(bear.ask('What is your name?'));
