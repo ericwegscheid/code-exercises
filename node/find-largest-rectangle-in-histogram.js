284ca1cf63b6eebe88ac0239f5e4624fad5d3986
Eric Wegscheid <ericwegscheid@gmail.com>
2018-08-04 20:02:49 -0500

    added find largest rectangle in histogram example in node

---
+++ node/find-largest-rectangle-in-histogram.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+
+const arr = [2, 3, 2, 0, 3, 5, 1, 1]; // result: 6
+// const arr = [2, 3, 2, 0, 3, 5, 1, 9]; // result: 9
+// const arr = [2, 0, 2, 0, 3, 0, 3, 2]; // result: 4
+
+let map = {};
+let index = 1;
+let resetIndex = 0;
+let largestArea = 0;
+
+arr.forEach((v, i) => {
+	i++;
+
+	if( !v ) {
+		resetIndex = i;
+	}
+
+	index = i - resetIndex;
+
+	// get all possible areas at this position
+	let possibleAreas = [v];
+
+	if( index > 1 ) {
+		possibleAreas.push(
+			// multiply min running value since resetIndex with index
+			Math.min.apply(this, (arr.slice(resetIndex, i))) * index
+		);
+	}
+
+	largestArea = Math.max(Math.max.apply(this, possibleAreas), largestArea);
+});
+
+console.log(largestArea);
