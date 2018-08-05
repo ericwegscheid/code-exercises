194354a13e86db271b2c993021465c480fd33e94
Eric Wegscheid <ericwegscheid@gmail.com>
2018-08-04 20:07:29 -0500

    clean up, removed unit-testing directory

--- node/unit-testing/index.js
+++
@@ -1,6 +0,0 @@
-var Bear = require('./src/bear');
-var bear = new Bear();
-
-console.log(bear.speak());
-console.log(bear.ask('What is your favorite food?'));
-console.log(bear.ask('What is your name?'));
