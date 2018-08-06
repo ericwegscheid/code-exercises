8872d4ca2ba3b90b5ca40ca2a42c6849a9cf9d80
Eric Wegscheid <ericwegscheid@gmail.com>
2018-08-05 22:03:07 -0500

    tightening up the fibonacci position problem

--- node/find-number-at-position-in-fibonacci-series.js
+++ node/find-number-at-position-in-fibonacci-series.js
@@ -4,17 +4,10 @@ const pos = process.argv[2] || 3;
 const arr = [0, 1];
 
 const step = () => {
-	let v = arr[pos];
-
-	if( v ) {
-		return v;
-	}
-
-	let lastIndex = arr.length -1;
-
-	arr.push(arr[lastIndex - 1] + arr[lastIndex]);
-
-	return step();
+	return arr[pos] || (() => {
+		arr.push(arr[arr.length - 2] + arr[arr.length - 1]);
+		return step();
+	})();
 };
 
 console.log(step());
