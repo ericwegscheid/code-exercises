9c2ffe311c79bfd3a522e953f01a70603b9d6c77
Eric Wegscheid <{ericwegscheid@gmail.com}>
2018-08-15 11:36:53 -0500

    added binary search example for node

---
+++ node/binary-search.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const input = process.argv[2] || 78;
+
+const arr = [1, 5, 7, 14, 18, 23, 27, 28, 33, 34, 35, 40, 49, 51, 52, 56, 60, 70, 77, 78, 79, 80, 81, 89, 90, 91, 92, 100];
+
+let found = false;
+let minIndex = 0;
+let maxIndex = arr.length - 1;
+let currentIndex;
+let currentValue;
+
+while( !found || maxIndex == minIndex ) {
+	currentIndex = Math.floor((maxIndex - minIndex) / 2) + minIndex;
+	currentValue = arr[currentIndex];
+
+	if( input > currentValue ) {
+		minIndex = currentIndex;
+
+	} else if( input < currentValue ) {
+		maxIndex = currentIndex;
+
+	} else {
+		found = true;
+	}
+}
+
+console.log(`found ${input} at index ${currentIndex}`);
