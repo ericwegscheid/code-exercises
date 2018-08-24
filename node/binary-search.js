16ed44b5f0964246dd1728bf0039496257e1ae1b
Eric Wegscheid <ericwegscheid@gmail.com>
2018-08-23 20:01:15 -0500

    node binary search example fixed

--- node/binary-search.js
+++ node/binary-search.js
@@ -4,32 +4,26 @@ const input = process.argv[2] || 78;
 
 const arr = [1, 5, 7, 14, 18, 23, 27, 28, 33, 34, 35, 40, 49, 51, 52, 56, 60, 70, 77, 78, 79, 80, 81, 89, 90, 91, 92, 100];
 
-let found = false;
-let minIndex = 0;
-let maxIndex = arr.length - 1;
-let currentIndex;
-let currentValue;
+const step = (min, max, prev) => {
+	let index = Math.floor((max - min) / 2) + min;
 
-while( !found ) {
-
-	debugger;
-
-	currentIndex = Math.floor((maxIndex - minIndex) / 2) + minIndex;
-	currentValue = arr[currentIndex];
-
-	if( [maxIndex, minIndex].indexOf(currentIndex) >= 0 ) {
-		break;
+	if( index === prev ) {
+		return -1;
 	}
 
-	if( input > currentValue ) {
-		minIndex = currentIndex;
+	let value = arr[index];
 
-	} else if( input < currentValue ) {
-		maxIndex = currentIndex;
+	if( input > value ) {
+		return step(index, max, index);
+
+	} else if( input < value ) {
+		return step(min, index, index);
 
 	} else {
-		found = true;
+		return index;
 	}
 }
 
-console.log(found ? `found ${input} at index ${currentIndex}` : `${input} does not exist in this array: \n${arr}`);
+let index = step(0, arr.length);
+
+console.log(index >= 0 ? `found ${input} at index ${index}` : `${input} does not exist in this array: \n${arr}`);
